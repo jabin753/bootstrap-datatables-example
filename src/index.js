075d5f9b4be5d5a60bfbf824da627e5440e76c48
jabin753 <jabin753@gmail.com>
2018-11-09 10:03:47 -0600

    Basic Table fetching

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var app = require('./app');
-var server = app.listen(app.get('port'),()=>{
+var server = app.listen(app.get('port'),() => {
 	var host = server.address().address;
     var port = server.address().port;
 	console.log('Servidor iniciado en %s:%s', host, port);
